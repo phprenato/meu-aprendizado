4eeaa3435e07956b230281dc8ed74d476bfbe933
phprenato <phprenato@gmail.com>
2026-03-26 20:19:08 -0300

    Aula 2: variaveis e sobre-mim

---
+++ sobre-mim.js
@@ -0,0 +1,11 @@
+let meuNome = "Renato";
+let minhaIdade = 40;
+let minhaCidade = "Cruz";
+let profissao = "Auxiliar de Produção";
+let estudando = "back-end com node.js";
+
+console.log("Meu nome é" , meuNome);
+console.log("Tenho" , minhaIdade, "anos");
+console.log("Moro em" , minhaCidade);
+console.log("Trabalho como" , profissao);
+console.log("Estou estudando" , estudando);
---
+++ variaveis.js
@@ -0,0 +1,10 @@
+//Minhas primeiras variáveis
+let nome = "Renato";
+let idade = 40;
+let estudante = true;
+let salario = 2000;
+
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+console.log("É estudante?", estudante);
+console.log("Salário:", salario);
