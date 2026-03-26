5077d1ca00b0422f0f16644a8ec859950c4fc3d8
phprenato <phprenato@gmail.com>
2026-03-26 20:51:37 -0300

    Aula 2: variaveis e sobre-mim

---
+++ idade.js
@@ -0,0 +1,5 @@
+let anoNascimento = 1986;
+let anoAtual = 2026;
+let idade = anoAtual - anoNascimento;
+
+console.log("Quem nasceu em" , anoNascimento, "Tem" , idade, "anos em", anoAtual);
---
+++ salario.js
@@ -0,0 +1,7 @@
+let salarioAtual = 2000;
+let aumento = 0.15; //15%
+let novoSalario = salarioAtual + (salarioAtual * aumento);
+
+console.log("Salário Atual: R$" , salarioAtual);
+console.log("Aumento de:" , aumento * 100,"%");
+console.log("Novo salário: R$" , novoSalario);
