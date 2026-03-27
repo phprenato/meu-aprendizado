f00b5296715a353ee9bb96f63112bddb6c21596e
phprenato <phprenato@gmail.com>
2026-03-26 21:31:42 -0300

    Exercicios extras - variaveis

---
+++ aprensentacao.js
@@ -0,0 +1,9 @@
+let nome = "Renato";
+let idade = 40;
+let cidade = "Cruz";
+let profissao = "Auxiliar de Produção.";
+
+let apresentacao = "Olá, meu nome é " + nome +", tenho " + idade + " moro em " + cidade + " e minha profissão é " + profissao;
+
+console.log(apresentacao);
+
